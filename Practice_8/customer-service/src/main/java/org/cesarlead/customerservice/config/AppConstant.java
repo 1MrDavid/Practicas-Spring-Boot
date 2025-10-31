7ca258dd1c2af5dd7cc9a77ca4ec6d382a303bf8
MrDavid <mrdavid1217@gmail.com>
2025-10-31 18:15:05 -0400

    Mas tarea

--- Practice_8/customer-service/src/main/java/org/cesarlead/customerservice/config/AppConstant.java
+++ Practice_8/customer-service/src/main/java/org/cesarlead/customerservice/config/AppConstant.java
@@ -5,6 +5,7 @@ public class AppConstant {
     public static final String LOG_ENTERING_METHOD = "Entrando al método: {}";
     public static final String LOG_EXITING_METHOD = "Saliendo del método: {}";
     public static final String LOG_USER_CREATED = "Usuario creado con ID: {}";
+    public static final String LOG_USER_CONSULTED = "Usuario con ID: {}, consultado";
     public static final String LOG_ERROR_CREATING_USER = "Error al crear usuario: {}";
 
     public AppConstant() {
--- Practice_8/customer-service/src/main/java/org/cesarlead/customerservice/controller/CustomerController.java
+++ Practice_8/customer-service/src/main/java/org/cesarlead/customerservice/controller/CustomerController.java
@@ -7,9 +7,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.cesarlead.customerservice.dto.CustomerDTO;
 import org.cesarlead.customerservice.dto.ErrorDetail;
 import org.cesarlead.customerservice.service.CustomerServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +20,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 
+import static org.cesarlead.customerservice.config.AppConstant.LOG_ENTERING_METHOD;
+import static org.cesarlead.customerservice.config.AppConstant.LOG_USER_CONSULTED;
+
 @RestController
 @RequestMapping("/api/v1/customers")
 @Tag(name = "administracion-cliente", description = "administracion de cliente")
+@Slf4j
 public class CustomerController {
+
+    // Legacy, escritura directa a pulmon
+    //private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 
     private final CustomerServiceImpl customerService;
 
@@ -64,8 +74,13 @@ public class CustomerController {
             @RequestParam Long id
     )
     {
+        log.info(LOG_ENTERING_METHOD, id);
 
         CustomerDTO response = customerService.findCustomerById(id);
+
+        log.debug("Respuesta recibida: " + response);
+
+        log.info(LOG_USER_CONSULTED, id);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
