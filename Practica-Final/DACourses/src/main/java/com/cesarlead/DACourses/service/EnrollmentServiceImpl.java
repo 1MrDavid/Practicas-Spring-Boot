f6a65bc5d5a874f3c7931f54c56a4717e97a2f79
MrDavid <mrdavid1217@gmail.com>
2025-11-13 10:24:12 +0000

    Validacion curso

--- Practica-Final/DACourses/src/main/java/com/cesarlead/DACourses/service/EnrollmentServiceImpl.java
+++ Practica-Final/DACourses/src/main/java/com/cesarlead/DACourses/service/EnrollmentServiceImpl.java
@@ -2,70 +2,71 @@ package com.cesarlead.DACourses.service;
 
 import com.cesarlead.DACourses.client.StudentFeignClient;
 import com.cesarlead.DACourses.dto.CrearInscripcionDTO;
+import com.cesarlead.DACourses.dto.CursoDTO;
 import com.cesarlead.DACourses.dto.EstudianteDTO;
 import com.cesarlead.DACourses.dto.InscripcionDTO;
 import com.cesarlead.DACourses.exception.ResourceNotFoundException;
 import com.cesarlead.DACourses.mapper.MapperEnrollment;
+import com.cesarlead.DACourses.model.Course;
 import com.cesarlead.DACourses.model.Enrollment;
+import com.cesarlead.DACourses.repository.CourseRepository;
 import com.cesarlead.DACourses.repository.EnrollmentRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class EnrollmentServiceImpl implements EnrollmentService {
 
-    private final StudentFeignClient studentFeignClient;
-    private final EnrollmentRepository enrollmentRepository;
-    private final MapperEnrollment mapper;
+  private final StudentFeignClient studentFeignClient;
+  private final EnrollmentRepository enrollmentRepository;
+  private final CourseService courseService;
+  private final MapperEnrollment mapper;
 
-    public EnrollmentServiceImpl(
-            StudentFeignClient studentFeignClient,
-            EnrollmentRepository enrollmentRepository,
-            MapperEnrollment mapper
-    ) {
-        this.studentFeignClient = studentFeignClient;
-        this.enrollmentRepository = enrollmentRepository;
-        this.mapper = mapper;
+  // Valida la existencia del estudiante
+  public boolean validateStudentById(Long id) {
+    try {
+      log.info("Solicitando informacion del estudiante con ID: " + id);
+
+      EstudianteDTO estudiante = studentFeignClient.getStudentByID(id);
+
+      log.info("Estudiante validado exitosamente: {} {}",
+          estudiante.nombre(), estudiante.apellido());
+
+      return true;
+
+    } catch (ResourceNotFoundException e) {
+      log.error("Estudiante no encontrado: {}", e.getMessage());
+      throw e;
+    } catch (Exception e) {
+      log.error("Error inesperado validando al estudiante: {}", e.getMessage());
+      throw new RuntimeException("Error inesperado validando al estudiante: " + e.getMessage());
     }
+  }
 
-    // Valida la existencia del estudiante
-    public boolean validateStudentById(Long id) {
-        try {
-            log.info("Solicitando informacion del estudiante con ID: " + id);
+  // Crea la inscripcion del curso
+  @Override
+  @Transactional
+  public InscripcionDTO createEnrollment(CrearInscripcionDTO request) {
 
-            EstudianteDTO estudiante = studentFeignClient.getStudentByID(id);
+    log.info("Validando la informacion del curso con ID: " + request.cursoId());
 
-            log.info("Estudiante validado exitosamente: {} {}",
-                    estudiante.nombre(), estudiante.apellido());
+    CursoDTO curso = courseService.findById(request.cursoId());
 
-            return true;
+    boolean existStudent = validateStudentById(request.estudianteId());
 
-        } catch (ResourceNotFoundException e) {
-            log.error("Estudiante no encontrado: {}", e.getMessage());
-            throw e;
-        } catch (Exception e) {
-            log.error("Error inesperado validando al estudiante: {}", e.getMessage());
-            throw new RuntimeException("Error inesperado validando al estudiante: " + e.getMessage());
-        }
-    }
+    Enrollment inscripcion = new Enrollment();
+    inscripcion.setCursoId(request.cursoId());
+    inscripcion.setEstudianteId(request.estudianteId());
+    inscripcion.setFechaInscripcion(LocalDateTime.now());
 
-    // Crea la inscripcion del curso
-    @Override
-    @Transactional
-    public InscripcionDTO createEnrollment(CrearInscripcionDTO request) {
-
-        boolean existStudent = validateStudentById(request.estudianteId());
-
-        Enrollment inscripcion = new Enrollment();
-        inscripcion.setCursoId(request.cursoId());
-        inscripcion.setEstudianteId(request.estudianteId());
-        inscripcion.setFechaInscripcion(LocalDateTime.now());
-
-        Enrollment savedEnrollment = enrollmentRepository.save(inscripcion);
-        return mapper.mapToInscripcionDTO(savedEnrollment);
-    }
+    Enrollment savedEnrollment = enrollmentRepository.save(inscripcion);
+    return mapper.mapToInscripcionDTO(savedEnrollment);
+  }
 }
