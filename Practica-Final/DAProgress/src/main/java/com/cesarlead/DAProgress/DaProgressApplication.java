90395f403666b44b23d9a9a3a8b5c2cfdc0ac33b
MrDavid <mrdavid1217@gmail.com>
2025-11-16 15:38:31 -0400

    Proyecto progreso

---
+++ Practica-Final/DAProgress/src/main/java/com/cesarlead/DAProgress/DaProgressApplication.java
@@ -0,0 +1,13 @@
+package com.cesarlead.DAProgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaProgressApplication.class, args);
+	}
+
+}
---
+++ Practica-Final/DAProgress/src/test/java/com/cesarlead/DAProgress/DaProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarlead.DAProgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
